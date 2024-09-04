4d97156711c36dce0994f9f04e09d7af431a3de4
User <jhonyriera@hotmail.com>
2024-09-03 19:08:18 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,11 @@
-import { provideRouter, Routes, withDebugTracing } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
-import { ApplicationConfig } from '@angular/core';
 import { FormpersonComponent } from './formperson/formperson.component';
 import { FormpetComponent } from './formpet/formpet.component';
 
-const appRoutes: Routes = [
-    { path: 'person', component:  FormpersonComponent},
-    { path: 'pet',      component: FormpetComponent },
-    { path: '/',      component: HomeComponent },
+export const routes: Routes = [
+    { path: 'person', component: FormpersonComponent },
+    { path: 'pet', component: FormpetComponent },
+    { path: '', component: HomeComponent },
     { path: '**', component: HomeComponent }
-  ];
-  export const appConfig: ApplicationConfig = {
-      providers: [provideRouter(appRoutes, withDebugTracing())]
-  }
+];
